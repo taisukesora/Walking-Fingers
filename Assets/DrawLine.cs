f5559dd9857219d7429c8520fb2c2a6539b14dc6
taisukesora <nagasuyusuke@gmail.com>
2014-03-15 20:17:28 +0900

    最初のコミット

---
+++ Assets/DrawLine.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawLine : MonoBehaviour {
+  LineRenderer line;
+  public Transform origin;
+  public Transform destination;
+
+  // Use this for initialization
+  void Start () {
+    line=GetComponent<LineRenderer>();
+  }
+	
+  // Update is called once per frame
+  void Update () {
+//    line.SetPosition(0, new Vector3(0, 0, 0));
+//    line.SetPosition(1, new Vector3(10, 10 ,10));
+    line.SetPosition(0, origin.position);
+    line.SetPosition(1, destination.position);
+  }
+}
---
+++ Assets/LeapMotion/Leap_Boilerplate/scripts_for_example_scenes/HandController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class HandController : MonoBehaviour {
+  public GameObject[] fingers;
+  public GameObject[] colliders;
+  public GameObject[] lines;
+
+  private LeapManager _leapManager;
+  // Use this for initialization
+  void Start () {
+    _leapManager = (GameObject.Find("LeapManager")as GameObject).GetComponent(typeof(LeapManager)) as LeapManager;
+  }
+	
+  // Update is called once per frame
+  void Update () {
+    Hand primeHand = _leapManager.frontmostHand();
+
+    if(primeHand.IsValid)
+      {
+	Vector3[] fingerPositions = _leapManager.getWorldFingerPositions(primeHand);
+	gameObject.transform.position = primeHand.PalmPosition.ToUnityTranslated();
+	if(gameObject.renderer.enabled != true) { gameObject.renderer.enabled = true; }
+	
+	for(int i=0;i<fingers.GetLength(0);i++)
+	  {
+	    if(i < fingerPositions.GetLength(0))
+	      {
+		fingers[i].transform.position = fingerPositions[i];
+		if(fingers[i].renderer.enabled == false) { fingers[i].renderer.enabled = true; }
+		if(lines[i].renderer.enabled == false) { lines[i].renderer.enabled = true; }
+		
+		if(colliders != null && i < colliders.GetLength(0))
+		  {
+		    (colliders[i].GetComponent(typeof(SphereCollider)) as SphereCollider).enabled = true;
+		  }
+	      }
+	    else
+	      {
+		fingers[i].renderer.enabled = false;
+		lines[i].renderer.enabled = false;
+		if(colliders != null && i < colliders.GetLength(0))
+		  {
+		    (colliders[i].GetComponent(typeof(SphereCollider)) as SphereCollider).enabled = false;
+		  }
+	      }
+	  }
+      }
+    else
+      {
+	gameObject.renderer.enabled = false;
+	
+	foreach(GameObject finger in fingers)
+	  {
+	    if(finger.renderer.enabled == true) { finger.renderer.enabled = false; }
+	  }
+	foreach(GameObject line in lines)
+	  {
+	    if(line.renderer.enabled == true) { line.renderer.enabled = false; }
+	  }
+      }
+    
+  }
+}
+
+
+
+
